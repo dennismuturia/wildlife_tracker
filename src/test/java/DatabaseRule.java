1f76a5c296d728dc5cb928f3bf6bbfff0287361b
dennismuturia <dmuturia2922@gmail.com>
2017-08-31 14:54:11 +0300

    Configuration of database
    I configured my postgresql's username and password. Then set the rules in the DB.java and also set the database rules to my preferences

---
+++ src/test/java/DatabaseRule.java
@@ -0,0 +1,18 @@
+import org.junit.rules.ExternalResource;
+import org.sql2o.*;
+
+public class DatabaseRule extends ExternalResource {
+
+  @Override
+  protected void before() {
+    DB.sql2o = new Sql2o("jdbc:postgresql://localhost:5432/wildlife_tracker_test", "muturia", "DontAsk12");
+  }
+
+  @Override
+  protected void after() {
+    try(Connection con = DB.sql2o.open()) {
+      String deleteAnimalsQuery = "DELETE FROM animals *;";
+      con.createQuery(deleteAnimalsQuery).executeUpdate();
+    }
+  }
+}
